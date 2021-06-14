ff508e8a4a86caf9c578d5f91d736e3b347d2031
norogoth <norogoth@gmail.com>
2021-06-14 14:05:34 -0700

    committing all my stuff again lel

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Fatman'},
+    {id: 14, name: 'Mr. Van Plastic'},
+    {id: 15, name: 'spidder guy'},
+    {id: 23, name: 'potato mouth'}
+]
